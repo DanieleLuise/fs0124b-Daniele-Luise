8ddf8b3db0465c215f17af52a99c6927f9c96cbf
DanieleLuise <luisedaniele03@gmail.com>
2024-04-15 20:33:03 +0200

    commit

--- mese4/settimana1/g1/esercizio1 mese4/src/Main.java
+++ mese4/settimana1/g1/esercizio1 mese4/src/Main.java
@@ -1,7 +1,40 @@
+import java.util.Arrays;
+
 public class Main {
+
+        public static int moltiplicazione(int a, int b)
+        {return a * b;};
+
+        public static String concat(String a,String b){
+            return a + b;
+        }
+
+        public static String[] inserisciArray(String[] array, String item){
+            String[] arrayNuovo = new String[6];
+            arrayNuovo[0] = a[0];
+
+
+
+
+        }
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+        System.out.println(moltiplicazione(13, 24));
+
+        System.out.println("concatenazione:"+ concat("hello,", "mario"));
+
+        String[] strConcatenazione2 = concatena2(new String[]{"ciao", "mi", "chiamo", "Daniele", "Luise"},b:"uee");
+        System.out.println(Arrays.tostring(strConcatenazione2));
+        }
+
 
 
 }
