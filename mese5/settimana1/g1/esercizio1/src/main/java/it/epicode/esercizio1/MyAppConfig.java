b3ee867f2fd103e67dde127ed489bde600e8e6d0
DanieleLuise <luisedaniele03@gmail.com>
2024-05-13 16:38:25 +0200

    commit

---
+++ mese5/settimana1/g1/esercizio1/src/main/java/it/epicode/esercizio1/MyAppConfig.java
@@ -0,0 +1,11 @@
+package it.epicode.esercizio1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyAppConfig {
+
+
+
+}
---
+++ mese5/settimana1/g1/esercizio1/src/main/java/it/epicode/esercizio1/Pizza.java
@@ -0,0 +1,10 @@
+package it.epicode.esercizio1;
+
+
+import lombok.Data;
+
+@Data
+public class Pizza {
+    private String pomodoro;
+    private String mozzarella;
+}
